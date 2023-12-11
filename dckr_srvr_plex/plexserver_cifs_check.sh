e7d7c41c69c03b11100043a3d71d446736a7308e
linw0724-linnet <150010675+linw0724-linnet@users.noreply.github.com>
2023-12-11 01:14:09 -0800

    code update

--- dckr_srvr_plex/plexserver_cifs_check.sh
+++ dckr_srvr_plex/plexserver_cifs_check.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Checks if Plex CIFS shares are mounted to local corresponding Plex Server directories
 if [ mountpoint -q /nas/plexserverdatabase ] || [ mountpoint -q /nas/plexserverdatabasebackup ] || [ mountpoint -q /nas/plexserverphysicalmedia ] || [ mountpoint -q /nas/plexservertempmedia ] || [ mountpoint -q /nas/plexservertranscode ] || [ mountpoint -q /nas/plexserveroptimizedmedia ]
-  # If Plex CIFS shares are mounted to local corresponding Plex Server directories, do nothing
   then
     :
-  # If Plex Database CIFS share are not mounted to local corresponding Plex Server directories, remount shares and restart Plex container
   else
     sudo mount -a; sudo docker restart plexserver
 fi
